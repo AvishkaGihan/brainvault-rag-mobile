22ed669b65bd8c140e478346f0b93c6cdeac05c6
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2025-12-26 08:32:17 +0530

    Update environment configuration and Gradle settings for improved compatibility

--- mobile/brainvault_app/android/app/build.gradle.kts
+++ mobile/brainvault_app/android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.brainvault.brainvault_app"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -27,8 +27,8 @@ android {
         applicationId = "com.brainvault.brainvault_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
-        targetSdk = 34
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
